419b398d82a554c8765ab83df0d9ee64e968d45a
Renzo Beltran <rnzbeltran@gmail.com>
2021-11-28 17:17:47 +0800

    Added fixes to convertion of text to number
    Squashed commit of the following:
    
    commit 4c2ecefc6662d9d1ca2623494e5ecf430b52d3e0
    Author: Renzo Beltran <rnzbeltran@gmail.com>
    Date:   Mon Nov 22 21:13:10 2021 +0800
    
        Added Procfile and Removed vercel.json
    
    commit db4c66c2038c0982c8e53b453a150a3dfd3bd982
    Author: Renzo Beltran <rnzbeltran@gmail.com>
    Date:   Mon Nov 22 20:47:34 2021 +0800
    
        Fixed routes added dest: "/app.py"
    
    commit a5f0254f46c5ee3a19040cbca14fb61514ef9b07
    Author: Renzo Beltran <rnzbeltran@gmail.com>
    Date:   Mon Nov 22 20:39:50 2021 +0800
    
        Changed vercel.json version from 1 to 2

--- app.py
+++ app.py
@@ -7,38 +7,132 @@ api = Api(app)
 word_args = reqparse.RequestParser()
 word_args.add_argument("word", type=str, help="Word is Required", required=True)
 
+
+
+
 class WordToNumber(Resource):
     def post(self):
+
+        def is_number(x):
+            if type(x) == str:
+                x = x.replace(',', '')
+            try:
+                float(x)
+            except:
+                return False
+            return True
+
         arg = word_args.parse_args()
 
         def text2int(textnum, numword={}):
+            units = [
+                "zero", "one", "two", "three", "four", "five", "six", "seven", "eight",
+                "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen",
+                "sixteen", "seventeen", "eighteen", "nineteen",
+            ]
+
+            tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+
+            scales = ["hundred", "thousand", "million", "billion", "trillion"]
+
+            ordinal_words = {'first':1, 'second':2, 'third':3, 'fifth':5, 'eighth':8, 'ninth':9, 'twelfth':12}
+
+            ordinal_endings = [('ieth', 'y'), ('th', '')]
+
             if not numword:
-                units = [
-                    "zero", "one", "two", "three", "four", "five", "six", "seven", "eight",
-                    "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen",
-                    "sixteen", "seventeen", "eighteen", "nineteen",
-                ]
-
-                tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
-
-                scales = ["hundred", "thousand", "million", "billion", "trillion"]
-
                 numword["and"] = (1,0)
                 for idx, word in enumerate(units): numword[word] = (1, idx)
                 for idx, word in enumerate(tens): numword[word] = (1, idx * 10)
                 for idx, word in enumerate(scales): numword[word] = (10 ** (idx * 3 or 2), 0)
 
+            textnum = textnum.replace('-', ' ')
+
             current = result = 0
-            for word in textnum.split():
-                scale, increment = numword[word]
-                current = current * scale + increment
-                if scale > 100:
-                    result += current
-                    current = 0
+            curstring = ''
+            onnumber = False
+            lastunit = False
+            lastscale = False
+
+            def is_numword(x):
+                if is_number(x):
+                    return True
+                if word in numword:
+                    return True
+                return False
+
+            def from_numword(x):
+                if is_number(x):
+                    scale = 0
+                    increment = int(x.replace(',', ''))
+                    return scale, increment
+                return numword[x]
+
             
-            return result + current
+
+            for word in textnum.split():
+                if word in ordinal_words:
+                    scale, increment = (1, ordinal_words[word])
+                    current = current * scale + increment
+                    if scale > 100:
+                        result += current
+                        current = 0
+                    onnumber = True
+                    lastunit = False
+                    lastscale = False
+                else:
+                    for ending, replacement in ordinal_endings:
+                        if word.endwith(ending):
+                            word = "%s%s" % (word[:-len(ending)], replacement)
+
+                    if (not is_numword(word)) or (word == 'and' and not lastscale):
+                        if onnumber:
+                            curstring += repr(result + current) + " "
+                        curstring += word + " "
+                        result = current = 0
+                        onnumber = False
+                        lastunit = False
+                        lastscale = False
+                    else:
+                        scale, increment = from_numword(word)
+                        onnumber = True
+
+                    if lastunit and (word not in scales):                                                                                                                                                                                                                                         
+                    # Assume this is part of a string of individual numbers to                                                                                                                                                                                                                
+                    # be flushed, such as a zipcode "one two three four five"                                                                                                                                                                                                                 
+                        curstring += repr(result + current)                                                                                                                                                                                                                                       
+                        result = current = 0
+
+                    if scale > 1:                                                                                                                                                                                                                                                                 
+                        current = max(1, current) 
+
+                    current = current * scale + increment                                                                                                                                                                                                                                         
+                    if scale > 100:                                                                                                                                                                                                                                                               
+                        result += current                                                                                                                                                                                                                                                         
+                        current = 0  
+
+                    lastscale = False                                                                                                                                                                                                              
+                    lastunit = False                                                                                                                                                
+                    if word in scales:                                                                                                                                                                                                             
+                        lastscale = True                                                                                                                                                                                                         
+                    elif word in units:                                                                                                                                                                                                             
+                        lastunit = True
+
+            if onnumber:
+                curstring += repr(result + current)
+
+            return curstring
+
+            #     scale, increment = numword[word]
+            #     current = current * scale + increment
+            #     if scale > 100:
+            #         result += current
+            #         current = 0
+            
+            # return result + current
         
         return {"integer": text2int(arg["word"])}
+
+
 
 api.add_resource(WordToNumber, '/WordsToNumber')
 
